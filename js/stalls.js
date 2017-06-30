1721d9209687f5611c048369d7bc6d9348d8baae
hideyarguelles <catherine_arguelles@dlsu.edu.ph>
2017-06-30 23:14:02 +0800

    Minor changes
    A lot of JS doesn't work idk why

---
+++ js/stalls.js
@@ -0,0 +1,7 @@
+// This is the JS for the stalls and products.
+
+$(document).ready(function () {
+    $(".list-group>li").on("hover", function () {
+        $(this).css("cursor", "pointer");
+    });
+});
