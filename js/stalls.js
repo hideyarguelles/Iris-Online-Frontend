cf5dd7cb6bfeb346dc78e081bfbe17851740ce53
hideyarguelles <catherine_arguelles@dlsu.edu.ph>
2017-07-05 14:49:16 +0800

    here
    it's new

--- js/stalls.js
+++ js/stalls.js
@@ -1,12 +1,11 @@
 // This is the JS for the stalls and products.
 
-(function ($) {
-
-    $('.spinner .btn:first-of-type').on('click', function() {
-        $('.spinner input').val( parseInt($('.spinner input').val(), 10) + 1);
-    });
-    $('.spinner .btn:last-of-type').on('click', function() {
-        $('.spinner input').val( parseInt($('.spinner input').val(), 10) - 1);
+$(() => {
+    $('.spinner .btn:first-of-type').on('click', function () {
+        $('.spinner input').val(parseInt($('.spinner input').val(), 10) + 1);
     });
 
-})(jQuery);
+    $('.spinner .btn:last-of-type').on('click', function () {
+        $('.spinner input').val(parseInt($('.spinner input').val(), 10) - 1);
+    });
+});
