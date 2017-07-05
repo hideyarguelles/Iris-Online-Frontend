2585caa75a75a80d17bc7efb73b6289a6fdb9166
hideyarguelles <catherine_arguelles@dlsu.edu.ph>
2017-07-05 15:33:58 +0800

    Updated Stalls JS

--- js/stalls.js
+++ js/stalls.js
@@ -1,9 +1,14 @@
 // This is the JS for the stalls and products.
 
 $(() => {
-    $('.card-purchase-div').keypress(function (key) {
-        if (key.charCode) {
-            
+    $('.card-purchase-div>input').keypress(function (key) {
+        // Small letter ASCII
+        if (key.charCode >= 97 && key.charCode <= 122) {
+            return false;
+        }
+        // Capital letter ASCII
+        else if (key.charCode >= 65 && key.charCode <= 90) {
+            return false;
         }
     });
 });
