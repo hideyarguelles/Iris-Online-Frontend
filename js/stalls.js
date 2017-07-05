bf6070f53c07d27d3d7cf2af9edcea2d62c4dff7
hideyarguelles <catherine_arguelles@dlsu.edu.ph>
2017-07-05 15:13:12 +0800

    Added changes
    I changed a lot

--- js/stalls.js
+++ js/stalls.js
@@ -1,11 +1,9 @@
 // This is the JS for the stalls and products.
 
 $(() => {
-    $('.spinner .btn:first-of-type').on('click', function () {
-        $('.spinner input').val(parseInt($('.spinner input').val(), 10) + 1);
-    });
-
-    $('.spinner .btn:last-of-type').on('click', function () {
-        $('.spinner input').val(parseInt($('.spinner input').val(), 10) - 1);
+    $('.card-purchase-div').keypress(function (key) {
+        if (key.charCode) {
+            
+        }
     });
 });
